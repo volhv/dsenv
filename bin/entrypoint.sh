a0ebdc06aee87c279602745d842224840ed42078
t <t@MacBook-Pro-t.local>
2022-03-23 15:31:31 +0300

    fix: support generic username

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "PATH=$PATH"
 
-export JUPYTER_RUNTIME_DIR="/home/me/"
+export JUPYTER_RUNTIME_DIR="/home/%%username%%/"
 
 # drop access to myuser and run cmd
 exec jupyter-notebook --ip=0.0.0.0 --port=8888\
      --FileCheckpoints.checkpoint_dir=/shared/notebooks/db/ipynb_checkpoints\
      --notebook-dir=/shared/notebooks/\
-     --certfile=/home/me/.local/share/jupyter/runtime/mycert.pem\
-     --keyfile=/home/me/.local/share/jupyter/runtime/mykey.key
+     --certfile=/home/%%username%%/.local/share/jupyter/runtime/mycert.pem\
+     --keyfile=/home/%%username%%/.local/share/jupyter/runtime/mykey.key
