b059f973da115bd5978d0de655434479d50903b8
volhv <tusaty@gmail.com>
2023-01-20 10:56:50 +0300

    complete user info with defaults

--- bin/env.sh
+++ bin/env.sh
@@ -7,9 +7,9 @@ export DEVENV_GPUS=0
 export DEVENV_HOME="/home/username"
 export DEVENV_NOTEBOOKS_HOME="/home/username/your/code"
 
-export DEVENV_USER="username"
-export DEVENV_USERID=<user_id>
-export DEVENV_USERGID=<user_gid>
+export DEVENV_USER="$USER"
+export DEVENV_USERID=`id -u`
+export DEVENV_USERGID=`id -g`
 export DEVENV_PWD=<password>
 export DEVENV_USERMAIL="email@example.com"
 export DEVENV_USERFULL="User Full Name"
